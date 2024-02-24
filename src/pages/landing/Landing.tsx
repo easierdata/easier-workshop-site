b39843630293bf31eaa9e04412cc64187a73a482
matthewnanas <spazeplayz@gmail.com>
2024-02-23 22:07:57 -0500

    Landing changes

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -7,13 +7,13 @@ export default function Landing(): JSX.Element {
     return (
         <div className="h-full grid md:grid-cols-2 ml-4 mr-4 md:ml-20 md:mr-20">
             <img src={group} alt="Landing" className="absolute top-20 right-0 w-1/2 -z-10" />
-            <div className="mt-20">
+            <div className="mt-8 md:mt-20">
                 <h1 className="font-bold text-4xl md:text-6xl">{LandingContent.heading}</h1>
                 <h2 className="mt-4 font-bold text-2xl md:text-3xl">{LandingContent.subheading}</h2>
                 <hr className="mt-4 mb-4"/>
                 <p className="text-lg mb-4">{LandingContent.description}</p>
 
-                <div className="sm:visible sm:grid sm:grid-rows-2 md:invisible md:h-0">
+                <div className="sm:visible sm:grid sm:grid-rows-2 md:invisible md:h-0 flex flex-col items-center">
                     <button
                         className="mt-3 px-6 py-2 drop-shadow-md rounded-3xl bg-[#039876] text-white hover:bg-[#038264]"
                         onClick={() => {
