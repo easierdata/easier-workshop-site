dfd9a1f841fe7e5e37cb3d1a8ff60a3f87f2016f
matthewnanas <spazeplayz@gmail.com>
2024-02-23 22:43:37 -0500

    Update location content

--- src/static/content.ts
+++ src/static/content.ts
@@ -9,7 +9,7 @@ export const AboutContent = {
 }
 
 export const LocationContent = {
-    "paragraph": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "paragraph": "The Workshop on Open Geospatial Science and the Decentralized Geospatial Web will be at 4600 River Road, College Park, MD 20740, room 325. The space is at the vibrant core of the University of Maryland's discovery district, an intellectual hub plastered with cutting-edge research, pioneering companies, and esteemed faculty. This space workshop will immerse us in an experience that capitalizes on the surrounding atmosphere to cultivate novel and vibrant ideas that bridge the gap between the decentralized geospatial web and open geospatial science. With the many transportation options such as rideshare services, taxis, buses, or the Metro rail system, getting to and from 4600 River road will be effortless."
 }
 
 export const speakers = [
